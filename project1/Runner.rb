7076381c465505cf7c2b2f2ffa9d1908ee93ac85
Ian Thompson <thompson.3623@osu.edu>
2021-02-01 16:59:42 -0500

    Added class definition, initialization method, and getter methods.

--- project1/Runner.rb
+++ project1/Runner.rb
@@ -1,1 +1,20 @@
 # Thompson.3623
+
+class Runner
+	# Scores for each player
+	@p1Score
+	@p1Score
+	#Constructor for class
+	def initialize
+		@p1Score = 0
+		@p2Score = 0
+	end
+	#Methods to get scores for each player
+	def p1Score
+		@p1Score
+	end
+	def p2Score
+		@p2Score
+	end
+
+end
