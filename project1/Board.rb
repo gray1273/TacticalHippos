be7acb535bf16cacb011d4cc27938cb3c581c895
DaqianZuo <zuo.115@osu.edu>
2021-02-04 13:43:38 +0800

    Update the containSet function

--- project1/Board.rb
+++ project1/Board.rb
@@ -1,3 +1,41 @@
 #Create Board object, check potential sets in the inUseCard.
 
 class Board
+
+  #print the in used cards
+  def printCurrentCards()
+    puts Deck.inUseCards
+  end
+
+  #method used to determine if any set exists, if not draw three more cards
+  def containSet(inUseCards)
+    @indicator = false
+    @indexOne = 0
+    while index < inUseCards.length && indicator!
+      @cardOne = inUseCards[indexOne]
+      @indexTwo = indexOne + 1
+      while indexTwo < inUseCards.length && indicator!
+        @cardTwo = inUseCards[indexTwo]
+        @indexThree = indexTwo + 1
+        while indexThree < inUseCards.length && indicator!
+          @cardThree = inUseCards[indexThree]
+          indicator = isSet(cardOne,cardTwo,cardThree)
+          if (indicator!)
+            indexThree += 1
+          end
+        end
+        if (indicator!)
+          indexTwo += 1
+        end
+      end
+      if (indicator!)
+        indexOne += 1
+      end
+    end
+
+    if (indicator!) #if indicator shows that we do not have a set
+      Deck.inUseCards.get(3)
+    end
+
+
+
