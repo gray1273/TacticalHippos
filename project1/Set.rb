7315224205c3515d0445e9e233efa990a9875772
Axay Patel <Patel.axay16@live.com>
2021-02-05 09:09:44 -0500

    wait until 's' to start and print rules added

--- project1/Set.rb
+++ project1/Set.rb
@@ -1,1 +1,27 @@
 # Project 1
+
+#require_relative 'runner'
+	#Waits until  s is pressed to start game
+	def waitUntilStart
+		require 'io/console'
+        readChar = ' ';
+        puts "Press 's' to start the game!"
+		while (readChar != 's') do
+			readChar = STDIN.getch
+		end
+    end
+
+    #Displays rules for game. ( https://en.wikipedia.org/wiki/Set_(card_game) )
+    def printRules
+        puts "\nRules:"
+        puts "A set consists of three cards satisfying all of these conditions:"
+        puts "They all have the same number or have three different numbers."
+        puts "They all have the same shape or have three different shapes."
+        puts "They all have the same shading or have three different shadings."
+        puts "They all have the same color or have three different colors."
+    end
+    
+    waitUntilStart()
+    printRules()
+    #TODO: Syntax errors in Runner
+    #Runner.waitUntilCallout()
