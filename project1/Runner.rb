34d8ab6c8e40ab0b497e49563c75c678b385e008
Ian Thompson <thompson.3623@osu.edu>
2021-02-05 16:13:04 -0500

    Fixed bug with Runner (used ++ instead of += 1).

--- project1/Runner.rb
+++ project1/Runner.rb
@@ -9,6 +9,7 @@ class Runner
 		@p1Score = 0
 		@p2Score = 0
 	end
+
 	#Methods to get scores for each player
 	def p1Score
 		@p1Score
@@ -16,17 +17,22 @@ class Runner
 	def p2Score
 		@p2Score
 	end
-	#Waits until either q or p is pressed (the button that either player uses to call out a set)
+
+	#Waits until either z or m is pressed (the button that either player uses to call out a set) or q (to quit)
 	def waitUntilCallout
 		# Source: https://stackoverflow.com/questions/39964008/read-a-single-char-from-stdin-without-pressing-enter/39967524
 		require 'io/console'
 		readChar = ' ';
-		while (readChar != 'q' && readChar != 'p') do
+		puts "Press [z] for Player 1 or [m] for Player 2. Press [q] to quit."
+		while (readChar != 'z' && readChar != 'm' && readChar != 'q') do
 			readChar = STDIN.getch
 		end
-		# TODO: change to the columns displayed
-		processCallout(readChar == 'q', 4)
+		if readChar != 'q' then
+			processCallout(readChar == 'z', 4)
+		end
+		return readChar != 'q'
 	end
+
 	# Gets input from the console of what set the player wants to input. Checks that set and increments if valid.
 	# Inputs:
 	# => player: true = p1, false = p2
@@ -34,29 +40,28 @@ class Runner
 	def processCallout(player, columns)
 		setString = ""
 		invalidInput = true
-		while invalidInput
-			echo "Enter three card names separated by spaces, e.g. \"A1 B2 C3\""
+		while invalidInput do
+			puts "Enter three card names separated by spaces, e.g. \"A1 B2 C3\""
 			setString = gets
 			setString.upcase
 			cardStrings = setString.split
 			cardStrings.each{ |s| 
-				if ((s[0] == 'A' || s[0] == 'B' || s[0] == 'C') && (s[1].to_i <= columns)
+				if ((s[0] == 'A' || s[0] == 'B' || s[0] == 'C') && (s[1].to_i <= columns)) then
 					invalidInput = false
 				end
 			}
-			if(invalidInput)
-				echo "Invalid input. Try again."
+			if invalidInput then
+				puts "Invalid input. Try again."
 			end
 		end
 		# TODO: call method that checks set
 		validSet = true
-		if(validSet)
-			if(player)
-				@p1Score++
-			else
-				@p2Score++
-			end
+		if (validSet && player) then
+			@p1Score += 1
+		elsif (validSet)
+			@p2Score += 1
 		else
-			echo "Sorry, that is not a valid set."
+			puts "Sorry, that is not a valid set."
 		end
+	end
 end
