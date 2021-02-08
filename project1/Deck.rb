3e40ccf698a800142e83ae49224b67525527682c
Ryan Bentivegna <ryanabentivegna@gmail.com>
2021-02-08 09:59:18 -0500

    Fixed the @ symbols and issues with instantiating class variables outside of methods

--- project1/Deck.rb
+++ project1/Deck.rb
@@ -4,53 +4,53 @@ require './Card.rb'
 
 class Deck
   
-  baseCards = Array.new
-  inUseCards = Array.new
-  usedCards = Array.new
+  @baseCards
+  @inUseCards 
+  @usedCards
   
   # Create the base Set deck
   def initialize()
-    3.times { |i| 3.times { |j| 3.times { |k| 3.times { |l|
-      baseCards.push Card.new i, j, k, l
-    }}}}
-
-    shuffle
-    get 12
+    reset
   end
 
   # Resets the class to the same state as initialization
   def reset()
-    baseCards = baseCards + inUseCards + usedCards
+    @baseCards = Array.new
+    3.times { |i| 3.times { |j| 3.times { |k| 3.times { |l|
+      @baseCards.push Card.new i, j, k, l
+    }}}}
+    @inUseCards = Array.new
+    @usedCards = Array.new
+
     shuffle
-    inUseCards = Array.new
-    usedCards = Array.new
+    get 12
   end
   
   # Shuffles baseCards (The whole deck, or remaining unseen cards)
   def shuffle
-    baseCards.shuffle
+    @baseCards.shuffle
   end
 
   # Removes cards from in-use
   # @param *cards: Variable argument, pass each card as a separate parameter
   def putBack(*cards)
-    inUseCards = inUseCards - cards
-    usedCards = usedCards + cards
+    @inUseCards = @inUseCards - @cards
+    @usedCards = @usedCards + @cards
   end
   
   # Adds a specified number of cards to the in-use
   # @param count How many cards
   # @return The cards removed
   def get(count = 3)
-    cards = baseCards[0, count]
-    inUseCards.concat cards
-    baseCards = baseCards - cards
-    return cards
+    @cards = @baseCards[0, count]
+    @inUseCards.concat @cards
+    @baseCards = @baseCards - @cards
+    return @cards
   end
   
   # Returns the cards that should be displayed on the screen
   def getCurrent()
-    return inUseCards
+    return @inUseCards
   end
 
 end
