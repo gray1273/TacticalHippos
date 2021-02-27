6c3ea9b34cbcea26889e78081c15d00841217306
DaqianZuo <zuo.115@osu.edu>
2021-02-27 17:16:59 +0800

    timer, locateCards, printAll features added

--- project2/runner.js
+++ project2/runner.js
@@ -1,83 +1,53 @@
-/*Runner class that handles the process of the game*/
+/*the function that takes the start time and difficulties then return the boolean showing whether the player finished on time or not*/
 
-
-
-class Runner {
-	var p1Score = 0;
-	var p2Score = 0;
-	var board = new Board();
-	
-	p1Score(){
-		return p1Score;
+function timer(startTime, difficulty){
+	var elapsedTime = new Date() - startTime;
+	elapsedTime /= 1000;
+	elapsedTime = Math.round(elapsedTime);
+	var withinTime;
+	if (difficuly == "easy"){
+		withinTime = elapsedTime <= 300;
 	}
-	
-	p2Score(){
-		return p2Score;
+	else if (difficulty == "medium"){
+		withinTime = elapsedTime <= 180;
 	}
+	else {
+		withinTime = elapsedTime < 60;
+	}
+	return withinTime;	
+}
 
-	waitUntilCall(startTime, difficulty){
-		while(!Board.containSet() && Board.DeckKernel.baseCards.length >= 3){
-			Board.DeckKernel.get(3);
-		}
-		if (!Board.containSet() && Board.DeckKernel.baseCards.length < 3){
-			alert("Game over! No set on the board and not enough cards to draw");
-			if (p1Score > p2Score){
-				alert("Player 1 wins! Final Score " + p1Score + " to " + p2Score);
-			}
-			else if (p1Score < p2Score){
-				alert("Player 2 wins! Final Score " + p1Score + " to " + p2Score);
-			}
-			else{
-				alert("It is a tie! Final Score: " + p1Score + " to " + p2Score);
-			}
-			return false;
-		}
-		var validInput = false;
-		var readChar;
-		while(!validInput){
-			readChar = window.prompt("Press [z] for player 1 or [m] for player 2. Press [q] to quit.");
-			validInput = true;
-			if (readChar != 'z' && readChar != 'm' && readChar != 'q'){
-				alert("Invalid input! Please Enter again!");
-				validInput = false;
-			}
-		}
-		elapsedTime = new Date() - startTime;
-		elapsedTime /= 1000;
-		elapsedTime = Math.round(elapsedTime);
-		var difficulty, withinTime;
-		if (difficuly == "easy"){
-			withinTime = elapsedTime <= 300;
-		}
-		else if (difficulty == "medium"){
-			withinTime = elapsedTime <= 180;
-		}
-		else {
-			withinTime = elapsedTime < 60;
-		}
-		if (readChar != 'q'){
-			processCallout (readChar == 'z');
-		}
-		else if (p1Score != p2Score){
-			if (p1Score > p2Score){
-				alert("Player 1 wins! Final Score " + p1Score + " to " + p2Score);
-			}
-			else (p1Score < p2Score){
-				alert("Player 2 wins! Final Score " + p1Score + " to " + p2Score);
-			}
-			if (withinTime){
-				alert("You also completed within time!");
-			}
-			else{
-				alert("Unfortunately, you could not complete withinin time");
-			}
-			alert("You took " + elapsedTime + " seconds.");
-		}
-			else{
-				alert("It is a tie! Final Score: " + p1Score + " to " + p2Score);
-			}
-		return readChar != 'q';
+/*the function that takes the clolum number, row number, and row length then locate the card object*/
 
+function locateCard(colNum, rowNum, rowLength){
+	var index = (rowNum - 1) * rowLength + colNum;
+	return DeckKernel.inUseCards[index];
+}
+
+/*the function that prints all the card in use*/
+
+function printAll(){
+	var index = 0;
+	var colNum = 1;
+	var rowNum = 1;
+	while(index < DeckKernel.inUseCards.length()){
+		rowNum = 1;
+		while (rowNum <= 3){
+			DeckKernel.inUseCards[index].printHTML(rowNum, ColNum);
+			rowNum += 1;
+		}
+		colNum += 1;
 	}
 }
 
+/*the function that draw three more cards if there is no set on board and we got enough cards. If we dont have enough cards to draw from return false*/
+
+function drawIfNoSet(){
+	if (!Board.containSet() && Board.deck.baseCards.length < 3){
+		return false;
+	}
+	while (!Board.containSet() && Board.deck.baseCards.length >= 3) {
+		Board.deck.get(3);	
+	}
+	return true;
+}
