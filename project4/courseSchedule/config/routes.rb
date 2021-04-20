b939ffbf988a0ed86b743000bbc2e7347188fbec
Ryan Bentivegna <ryanabentivegna@gmail.com>
2021-04-19 22:36:41 -0400

    Added initial stuff for grader_assignment page

--- project4/courseSchedule/config/routes.rb
+++ project4/courseSchedule/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'search', to: 'search#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  devise_for :user
-  devise_scope :user do
-    root :to => "devise/sessions#new"
-  end
+  devise_for :users
+  get 'assign', to: 'grader_assignment#index'
+  get 'search', to: 'search#index'
+  root to: 'search#index'
+  #devise_scope :user do
+  #  root :to => "devise/sessions#new"
+  #  get 
+  #end
 end
