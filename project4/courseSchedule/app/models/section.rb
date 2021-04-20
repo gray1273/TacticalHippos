73c1c48fbac11ad6d3d820e05865c6806ae8e673
DaqianZuo <zuo.115@osu.edu>
2021-04-20 20:30:35 +0800

    Added some scopes for filtering

--- project4/courseSchedule/app/models/section.rb
+++ project4/courseSchedule/app/models/section.rb
@@ -7,6 +7,13 @@ class Section < ApplicationRecord
 	belongs_to :location
 	has_many :users, through: :grader_sections
 
+	scope :filter_by_course, -> (course) { where("course LIKE ?", "#{name}%") }
+	scope :filter_by_instructor, -> (instructor) { where instructor: instructor }
+	scope :filter_by_section_type, -> (section_type) { where(section_type: section_type}
+	scope :filter_by_term, -> (term) { where term: term }
+	scope :filter_by_instruction_mode, -> (instruction_mode) { where instruction_mode: instruction_mode }
+	scope :filter_by_location, -> (location) { where location: location }
+
 	accepts_nested_attributes_for :course
 	accepts_nested_attributes_for :instructor
 	accepts_nested_attributes_for :section_type
