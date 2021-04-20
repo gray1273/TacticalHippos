1c7d67f4f0d2e4f570dcf2522e08676ea0b51da8
Ian Thompson <thompson.3623@osu.edu>
2021-04-20 14:08:21 -0400

    Lots of changes

--- project4/courseSchedule/app/Scraper.rb
+++ project4/courseSchedule/app/Scraper.rb
@@ -1,14 +1,12 @@
 #Create webscraper
 require 'rubygems'
 require 'nokogiri'
-require './ClassesObject.rb'
-require './SectionObject.rb'
+require_relative 'ClassesObject.rb'
+require_relative 'SectionObject.rb'
 require 'watir'
 require 'webdrivers'
-require './models/course.rb'
-require './models/section.rb'
-require './models/user.rb'
-require './models/season.rb'
+#https://stackoverflow.com/questions/5942281/ruby-scripts-with-access-to-rails-models
+require_relative '../config/environment.rb'
 
 
 #Given a section, updates it to the webscraped information
@@ -18,8 +16,8 @@ def updateSection(section, updater)
     section.section_name = updater.sectionNumber
     season = Season.find_or_create_by title: updater.term
     section.term_id = Term.find_or_create_by year: updater.year, season_id: season
-    section.instructor_id = (User.find_or_create_by first_name: updater.professor) or nil
-    section.days_of_week = updater.days
+    section.instructor_id = (User.find_or_create_by first_name: updater.instructor) or nil
+    section.daysOfWeek = updater.days
     #section.location = Location.find_or_create_by location: updater.building + updater.room
 
     section.save
@@ -27,11 +25,11 @@ end
 
 #Given a course, updates it to the webscraped information
 def updateCourse(course, updater)
-    course.title = updater.className
-    course.course_name = updater.classNumber
+    course.title = updater.courseTitle
+    course.course_name = updater.courseName
     course.description = updater.description
     
-    updater.classSections.each do |newSection|
+    updater.courseSections.each do |newSection|
         section = course.sections.find_by section_name: newSection.sectionNumber
         if section.blank? then
             section = Section.new
@@ -46,7 +44,7 @@ end
 #Adds information to the model
 def addToModel(courses)
     courses.each do |newCourse|
-        course = Course.find_by title: newCourse.className
+        course = Course.find_by title: newCourse.courseTitle
         if course.blank? then
             updateCourse(Course.new, newCourse)
         else
@@ -70,52 +68,187 @@ def scrapeWebsite(campus, term, depart)
 	browser.element(css: "h1.loading-courses").wait_until_present
 	#div.course.ng-scope
 	
+	sectionController = SectionsController.new
 	
-	
-	classInfoArray = Array.new
+	#classInfoArray = Array.new
 	parseMain = Nokogiri::HTML(browser.html)
-	totalClasses = Array.new
+	totalCourses = Array.new
 	
-	#Find all the classes on the page
-	totalClasses = parseMain.css('div.course.ng-scope')
-	totalClasses.each do |indivClass|
+	#Find all the courses on the page
+	totalCourses = parseMain.css('div.course.ng-scope')
+	totalCourses.each do |individualCourse|
 		
-		#get basic info for each class
-		classNumber= indivClass.css('h3.ng-binding').text.strip
-		className= indivClass.css('span.course-title.ng-binding').text.strip
-		#Makes sure only course code is in the attribute classNumber
-		classNumber = classNumber[0..9]
-		#Adds a new location for description so it only gets this 'p.ng-binding' css element
-		descriptionLocation = indivClass.css('div.col-md-12.light.course-info')
-		description= descriptionLocation.css('p.ng-binding').text.strip
-		classSections= indivClass.css('div.section-container.ng-scope')
-		numberOfSections = classSections.length
-		classInfo = ClassesObject.new(classNumber, className, description, numberOfSections)
+		#this tag has multiple things that we need so we split it up
+		courseHeader = individualCourse.css('h3.ng-binding').text.strip.split("\n")
+		courseName = courseHeader[0]
+		courseTitle = courseHeader[1]
+		courseDescription = individualCourse.css('div.col-md-12.light.course-info').css('p.ng-binding').text.strip
 		
-		#Loops through each section that a class has
-		classSections.each do |classSection|
-			
-			sectionNumber= classSection.css('h4.subtitle.ng-binding').text.strip
-			year= classSection.css('span.label.primary.ng-binding').text.strip
-			term= classSection.css('span.darker.ng-binding').text.strip
+		if courseName.nil?
+			courseName = ""
+		end
+		if courseTitle.nil?
+			courseTitle = ""
+		else
+			#https://ruby-doc.org/core-3.0.1/String.html#method-i-lstrip
+			courseTitle = courseTitle.lstrip
+		end
+		if courseDescription.nil?
+			courseDescription = ""
+		end
+
+		courseSections = individualCourse.css('div.section-container.ng-scope')
+		numberOfSections = courseSections.length
+		course = Course.find_or_create_by(:course_name => courseName, :title => courseTitle, :description => courseDescription)
+		# puts course.course_name + "\t" + course.title + "\t" + course.description[0..25]
+		#classInfo = ClassesObject.new(courseName, courseTitle, courseDescription, numberOfSections)
+		
+		#Loops through each section that a course has
+		courseSections.each do |individualSection|
+			sectionHeader = individualSection.css('h4.subtitle.ng-binding').text.strip.split("\n")
+			#e.g. "Lecture"
+			sectionType = sectionHeader[1]
+			#e.g. "Section 0010 (5088)"
+			if sectionType.nil?
+				sectionType = ""
+			else
+				#https://ruby-doc.org/core-3.0.1/String.html#method-i-lstrip
+				sectionType = sectionType.lstrip
+			end
+
+			numberInfo = sectionHeader[2].split(" ")
+			sectionNumber = numberInfo[1]
+			if sectionNumber.nil?
+				sectionNumber = 0
+			else
+				sectionNumber = sectionNumber.to_i
+			end
+			classNumber = numberInfo[2]
+			if classNumber.nil?
+				classNumber = 0;
+			else
+				classNumber = classNumber[1..numberInfo[2].length-1].to_i
+			end
+
+			sectionTerm = individualSection.css('span.label.primary.ng-binding').text.strip
+
+			instructionMode = individualSection.css('p.ng-binding').text.strip.split("\n")[0].split(":")[1].lstrip
+
 			#Gets info from the meeting division with time and location specifics
-			meetingInfo= classSection.css('div.meeting.ng-scope')
-				professor= meetingInfo.css('li.right.ng-binding.ng-scope').text.strip
-				time= meetingInfo.css('div.meeting-times.ng-binding').text.strip
-				days= meetingInfo.css('p.sr-only.sr-only-focusable.ng-binding').text.strip
-				building= meetingInfo.css('p.ng-binding').text.strip
-				room= meetingInfo.css('span.ng-binding.ng-scope').text.strip
-				section = SectionObject.new(sectionNumber,year,term,professor,time,days,building,room)
-			#Appends SectionObject to the array in its ClassesObject
-			classInfo.classSections.append(section)
+			meetingInfo = individualSection.css('div.meeting.ng-scope')
+
+			instructor = meetingInfo.css('li.right.ng-binding.ng-scope').text.strip.split(" ")
+			instructorLastName = instructor.pop
+			instructorFirstName = instructor.join(" ")
+
+			days= meetingInfo.css('p.sr-only.sr-only-focusable.ng-binding').text.strip
+			daysOfWeek = ""
+			#https://stackoverflow.com/questions/8258517/how-to-check-whether-a-string-contains-a-substring-in-ruby/8258571
+			if days.include? "Sunday"
+				daysOfWeek += "Su"
+			end
+			if days.include? "Monday"
+				daysOfWeek += "M"
+			end
+			if days.include? "Tuesday"
+				daysOfWeek += "Tu"
+			end
+			if days.include? "Wednesday"
+				daysOfWeek += "W"
+			end
+			if days.include? "Thursday"
+				daysOfWeek += "Th"
+			end
+			if days.include? "Friday"
+				daysOfWeek += "F"
+			end
+			if days.include? "Saturday"
+				daysOfWeek += "Sa"
+			end
+
+			location= meetingInfo.css('p.ng-binding').text.strip.split("\n")[0]
+
+			dates = meetingInfo.css('div.meeting-dates.ng-binding').text.strip.split(" - ")
+			startDate = dates[0].split("/")
+			startDateMonth = -1
+			startDateDay = -1
+			startDateYear = -1
+			unless startDate.nil?
+				startDateMonth = startDate[0].to_i
+				startDateDay = startDate[1].to_i
+				startDateYear = startDate[2].to_i
+			end
+
+			endDate = dates[1].split("/")
+			endDateMonth = -1
+			endDateDay = -1
+			endDateYear = -1
+			unless endDate.nil?
+				endDateMonth = endDate[0].to_i
+				endDateDay = endDate[1].to_i
+				endDateYear = endDate[2].to_i
+			end
+
+			times = meetingInfo.css('div.meeting-times.ng-binding').text.strip.split(" - ")
+			startTime = times[0]
+			startTimeHour = -1
+			startTimeMinute = -1
+			unless startTime.nil?
+				startTime = startTime.strip.split(" ")
+				clockTime = startTime[0].split(":")
+				amPm = startTime[1]
+				startTimeHour = clockTime[0].to_i
+				#https://stackoverflow.com/questions/21331057/string-equalsignorecase-equivalent-in-ruby
+				if !amPm.nil? && amPm.casecmp("pm")
+					startTimeHour += 12
+				end
+				startTimeMinute = clockTime[1].to_i
+			end
+			endTime = times[1]
+			endTimeHour = -1
+			endTimeMinute = -1
+			unless endTime.nil?
+				endTime = endTime.strip.split(" ")
+				clockTime = endTime[0].split(":")
+				amPm = endTime[1]
+				endTimeHour = clockTime[0].to_i
+				#https://stackoverflow.com/questions/21331057/string-equalsignorecase-equivalent-in-ruby
+				if !amPm.nil? && amPm.casecmp("pm")
+					endTimeHour += 12
+				end
+				endTimeMinute = clockTime[1].to_i
+			end
+			
+			puts "#{startTimeHour}:#{startTimeMinute}-#{endTimeHour}:#{endTimeMinute}|#{startDateYear}/#{startDateMonth}/#{startDateDay}-#{endDateYear}/#{endDateMonth}/#{endDateDay}"
+			sectionController.create_safely(course,
+											sectionNumber,
+											classNumber,
+											instructorFirstName,
+											instructorLastName,
+											sectionType,
+											sectionTerm,
+											instructionMode,
+											location,
+											startDateYear,
+											startDateMonth,
+											startDateDay,
+											endDateYear,
+											endDateMonth,
+											endDateDay,
+											daysOfWeek,
+											startTimeHour,
+											startTimeMinute,
+											endTimeHour,
+											endTimeMinute)
+			# classInfo.courseSections.append(section)
 		end
 		#Adds the ClassesObject to the output array
-		classInfoArray.append(classInfo)	
+		#classInfoArray.append(classInfo)	
 	end	
 	
 	#puts parseMain
 	#puts classInfoArray.length
-return classInfoArray
+#return classInfoArray
 end
 
 =begin
@@ -133,25 +266,26 @@ end
 #Campus = col
 #term = 1214 for summer 2021
 #depart = cse, ece, ETC
-temp = scrapeWebsite("col", "1214", "cse") #NOTE: All 3 arguments must be strings!!
+#temp = 
+scrapeWebsite("col", "1214", "cse") #NOTE: All 3 arguments must be strings!!
 #temp.each do |course|
-#	puts course.classNumber
+#	puts course.courseName
 #end
-addToModel(temp)
+#addToModel(temp)
 
-#puts "Class Number: " + temp[1].classNumber
-#puts "Class Name: " + temp[1].className
+#puts "Class Number: " + temp[1].courseName
+#puts "Class Name: " + temp[1].courseTitle
 #puts "Class Description: " + temp[1].description
-#puts temp[1].classSections.length
-#numberOfSections can be used to run a for loop for each classSection
-#puts temp[1].classSections[0].sectionNumber
-#puts temp[1].classSections[0].year
-#puts temp[1].classSections[0].term
-#puts temp[1].classSections[0].professor
-#puts temp[1].classSections[0].time
-#puts temp[1].classSections[0].days
-#puts temp[1].classSections[0].building
-#puts temp[1].classSections[0].room
+#puts temp[1].courseSections.length
+#numberOfSections can be used to run a for loop for each individualSection
+#puts temp[1].courseSections[0].sectionNumber
+#puts temp[1].courseSections[0].year
+#puts temp[1].courseSections[0].term
+#puts temp[1].courseSections[0].instructor
+#puts temp[1].courseSections[0].time
+#puts temp[1].courseSections[0].days
+#puts temp[1].courseSections[0].building
+#puts temp[1].courseSections[0].room
 
 
 
