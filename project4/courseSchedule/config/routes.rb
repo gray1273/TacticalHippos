1c1bcfaac751f8a17fa0a6391b594fc81b91a9dc
Ryan Bentivegna <ryanabentivegna@gmail.com>
2021-04-22 09:26:20 -0400

    USER TYPES WORKgit status!

--- project4/courseSchedule/config/routes.rb
+++ project4/courseSchedule/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
   get 'assign', to: 'grader_assignment#index'
   get 'search', to: 'search#index'
   get 'my_applications', to: 'my_grader_applications#index'
