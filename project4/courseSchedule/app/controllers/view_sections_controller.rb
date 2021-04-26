8de8e55c6d4a53e69296a33286a26bd60ffa4141
tripathisrujan <tripathisrujan.58@buckeyemail.osu.edu>
2021-04-26 01:30:35 -0400

    patch

--- project4/courseSchedule/app/controllers/view_sections_controller.rb
+++ project4/courseSchedule/app/controllers/view_sections_controller.rb
@@ -1,6 +1,6 @@
 class ViewSectionsController < ApplicationController
     def index
-        puts user.user_type
+        puts "User type" + user.user_type
         @user_type = user.user_type
     end
 end
