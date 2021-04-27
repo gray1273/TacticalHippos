38eb20ce11c215848a7503f953443dd1853ead39
tripathisrujan <tripathisrujan.58@buckeyemail.osu.edu>
2021-04-27 06:04:08 -0400

    patch

--- project4/courseSchedule/app/controllers/grader_assignment_controller.rb
+++ project4/courseSchedule/app/controllers/grader_assignment_controller.rb
@@ -1,24 +1,14 @@
 class GraderAssignmentController < ApplicationController
-
-
-    #Should eventually let them specifically search the courses
     def index
-        puts user_signed_in?
-        puts current_user
-        puts current_user.user_type_id
-        puts current_user.user_type
-        puts "printed"
-        #if user_signed_in? then
-            #TODO: Add a list of all applications and let admin accept/reject them
-        #else
-            #redirect_to "search"
-        #end
+        if user_signed_in?
+            @data = Course.all
+        end
     end
 
     #Handle grader assignments from client
     def edit
         section = GraderSection.find_by course_id: params[:course], section_id: params[:section], grader_id: params[:id]
-        if params[:accepted] then 
+        if params[:accepted] then
             section.status = :accepted
         else
             section.status = :rejected
