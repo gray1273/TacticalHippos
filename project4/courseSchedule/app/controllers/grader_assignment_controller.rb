6156f67638fd9f7955576de28e8dc9954545a160
tripathisrujan <tripathisrujan.58@buckeyemail.osu.edu>
2021-04-27 08:28:23 -0400

    patch

--- project4/courseSchedule/app/controllers/grader_assignment_controller.rb
+++ project4/courseSchedule/app/controllers/grader_assignment_controller.rb
@@ -1,18 +1,20 @@
 class GraderAssignmentController < ApplicationController
     def index
         if user_signed_in?
-            @data = Course.all
+            @courses = Course.all
+            @graders = User.where(:user_type.title => "Grader")
         end
     end
 
     #Handle grader assignments from client
     def edit
-        section = GraderSection.find_by course_id: params[:course], section_id: params[:section], grader_id: params[:id]
-        if params[:accepted] then
-            section.status = :accepted
-        else
-            section.status = :rejected
-        end
-        section.save
+        puts params[:section_id]
+        # section = GraderSection.find_by course_id: params[:course], section_id: params[:section], grader_id: params[:id]
+        # if params[:accepted] then
+        #     section.status = :accepted
+        # else
+        #     section.status = :rejected
+        # end
+        # section.save
     end
 end
